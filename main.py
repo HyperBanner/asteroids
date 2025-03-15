7292a7002f9e80b17e2a6feecdaea29a241aaa81
HyperBanner <131620724+HyperBanner@users.noreply.github.com>
2025-03-15 12:58:34 +0100

    Added Player to groups
    Added the Player class to groups "updatables" and
    "drawables", and used those in the game loop for
    easier management of game objects for later.
    
    Also fixed an issue with accidentally calling
    clock.tick() twice, so I also updated the
    PLAYER_SPEED constant, because the game runs
    twice as fast now.

--- main.py
+++ main.py
@@ -9,17 +9,20 @@ def main():
     clock = pygame.time.Clock()
     dt = 0
 
-    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+    updatables = pygame.sprite.Group()
+    drawables = pygame.sprite.Group()
+    Player.containers = (updatables, drawables)
+    Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
 
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill("black")
-        player.update(dt)
-        player.draw(screen)
+        updatables.update(dt)
+        for drawable in drawables:
+            drawable.draw(screen)
         pygame.display.flip()
-        clock.tick(60)
         dt = clock.tick(60) / 1000
 
 
