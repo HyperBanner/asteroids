ee8a4377895d685858d047d453e2b3f98c97b970
HyperBanner <131620724+HyperBanner@users.noreply.github.com>
2025-11-28 20:49:52 +0100

    Player is rendered correctly

--- main.py
+++ main.py
@@ -1,24 +1,32 @@
 import pygame
 from constants import SCREEN_WIDTH, SCREEN_HEIGHT
 from logger import log_state
+from player import Player
 
 
 def main():
+    # print some params to the console on startup
     print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
 
+    # main variables and objects
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-    clock = pygame.time.Clock()
     dt = 0
+    clock = pygame.time.Clock()
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
 
     while True:
+        # make quit window button work
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+
+        # game loop
         log_state()
         screen.fill("black")
+        player.draw(screen)
         pygame.display.flip()
         dt = clock.tick(60) / 1000
 
