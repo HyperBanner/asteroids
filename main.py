20149427d4b1dd48d5b0fbcec6fe18c0dfd88b68
HyperBanner <131620724+HyperBanner@users.noreply.github.com>
2025-11-28 20:20:35 +0100

    Delta time works, game runs at 60 fps

--- main.py
+++ main.py
@@ -4,11 +4,15 @@ from logger import log_state
 
 
 def main():
-    pygame.init()
-    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
+
+    pygame.init()
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
+
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
@@ -16,6 +20,7 @@ def main():
         log_state()
         screen.fill("black")
         pygame.display.flip()
+        dt = clock.tick(60) / 1000
 
 
 if __name__ == "__main__":
